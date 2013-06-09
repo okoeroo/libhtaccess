0a1a569936987131262cf1c8b672fc8ffbdba3fd
Oscar Koeroo <okoeroo@gmail.com>
2013-06-09 22:27:01 +0200

    Updating tree.h and queue.h.

--- src/htaccess/htaccess.h
+++ src/htaccess/htaccess.h
@@ -1,6 +1,10 @@
 #ifndef HTACCESS_H
     #define HTACCESS_H
 
+typedef struct lhta_ctx_s {
+
+} lhta_ctx_t;
+
 int lhta_parse_and_load(const char *buf);
 char *lhta_readfileintobuffer(const char *fname);
 
